a5e98576bd3282ccfbdafac4eaa8cc1c5e271317
luisnvrr <luis.a.navarro@gmail.com>
2023-04-25 14:17:57 -0300

    Commit Incial Lista de Tareas

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route("/new")
+def new_task():
+    return render_template("task.html")
+@app.route("/all")
+def all_task():
+    return render_template("all_tasks.html")
+
+app.run(debug=True)
