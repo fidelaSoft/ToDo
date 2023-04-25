41d38d489b2bdb4059baaac68e82b067b80b5db3
484axel484 <axelrodrigoaguero@hotmail.com>
2023-04-25 16:36:56 -0300

    PantalladeTareaNueva

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
 @app.route("/new")
 def new_task():
     return render_template("task.html")
+
 @app.route("/all")
 def all_task():
     return render_template("all_tasks.html")
