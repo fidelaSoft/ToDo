77abbf28859233fd278296a5b95c183ce6ebe88a
Lucas <lucascasado302000@gmail.com>
2023-05-02 18:47:19 -0300

    insert DB

--- app.py
+++ app.py
@@ -1,18 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
+
+from db.dbTask import ListTaskDataBase
+from model.Task import Task
 
 app = Flask(__name__)
+
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/new")
 def new_task():
     return render_template("task.html")
 
+
+# -----------  Insert new Task -----------
+@app.route('/new/insertTask', methods=['POST'])
+def saveInsertTask():
+    db = ListTaskDataBase()
+    if request.method == 'POST':
+        state = 'En Proceso'
+
+        task = Task(title=request.form.get("title"), description=request.form.get("description"),
+                    responsible=request.form.get("responsible"), state=state)
+
+        db.insertTask(task=task)
+        # flash(' Tarea Registrada.', 'success')
+
+    return redirect(url_for('home'))
+
+
 @app.route("/all")
 def all_task():
     return render_template("all_tasks.html")
+
 
 app.run(debug=True)
--- db/dbTask.py
+++ db/dbTask.py
@@ -1,13 +1,10 @@
 import psycopg2
-
 from model import Task
 
 
 class ListTaskDataBase:
     def __init__(self):
-        self.connection = psycopg2.connect(host="localhost", database="lisTask", user="postgres",
-                                           port="5432",
-                                           password="colocar password")
+        self.connection = psycopg2.connect(host="localhost", database="listTask", user="postgres", port="5432", password="lucas3030")
         self.cursor = self.connection.cursor()
 
     # All TASK
